180e9a9c613c6b8e6513b5b8935c195c9bba4784
aungnayhtetoo <aungnayhtetoo37@gmail.com>
2020-11-19 00:35:34 -0500

    showDetail and showDetailInPlace methods are added. Added photos for all the students in the table.

--- src/controller/DetailedModelViewController.java
+++ src/controller/DetailedModelViewController.java
@@ -10,8 +10,13 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import model.Student;
 
 /**
  * FXML Controller class
@@ -30,17 +35,45 @@ public class DetailedModelViewController implements Initializable {
     private ImageView imageViewer;
 
     @FXML
-    void goBackScene(ActionEvent event) {
+    private Button backBtn;
     
+    Student selectedModel;
+    Scene previousScene;
+    
+    @FXML
+    void goBackScene(ActionEvent event) {
+        //current stage from backBtn
+        Stage stage = (Stage)backBtn.getScene().getWindow();
+        
+        if(previousScene != null){
+            stage.setScene(previousScene);
+        }
     }
     
-
+    public void setPreviousScene(Scene scene){
+        previousScene = scene;
+        backBtn.setDisable(false);
+    }
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        backBtn.setDisable(true);
     }    
+
+    void initData(Student studentSelected) {
+        selectedModel = studentSelected;
+        nameText.setText(selectedModel.getFName() + " " + selectedModel.getLName());
+        studentIdText.setText(selectedModel.getStudentId().toString());
+        
+        try{
+            String imgName = "/resource/images/Crop/" + selectedModel.getStudentId().toString() + ".jpg";
+            Image profilePic = new Image(getClass().getResourceAsStream(imgName));
+            imageViewer.setImage(profilePic);
+        } catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
     
 }
